48f7b2f02aa3fbde782dac16aa5477aa8a0851f8
cbepxbeo <cbepxbeo@mail.ru>
2023-07-18 04:36:51 +0300

    Create MultipartFormDataSharedTests.swift

---
+++ Tests/MultipartFormDataTests/MultipartFormDataSharedTests/MultipartFormDataSharedTests.swift
@@ -0,0 +1,15 @@
+/*
+ 
+ Project: MultipartFormData
+ File: MultipartFormDataSharedTests.swift
+ Created by: Egor Boyko
+ Date: 18.07.2023
+ 
+ Status: #In progress | #Not decorated
+ 
+ */
+
+import XCTest
+@testable import MultipartFormData
+
+final class MultipartFormDataSharedTests: XCTestCase {}

c90bf4a39877c13069b6eb47d2cd4945540e5e63
cbepxbeo <cbepxbeo@mail.ru>
2023-07-18 05:31:05 +0300

    Create Boundary+Static+Property+Max.swift

---
+++ Sources/Boundary/Boundary+Static+Property+Max.swift
@@ -0,0 +1,14 @@
+/*
+ 
+ Project: MultipartFormData
+ File: Boundary+Static+Property+Max.swift
+ Created by: Egor Boyko
+ Date: 18.07.2023
+ 
+ Status: #Completed | #Not decorated
+ 
+ */
+
+extension Boundary {
+    static let max: Int = 70
+}
